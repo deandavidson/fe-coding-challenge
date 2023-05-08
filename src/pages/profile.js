9bbedb1e9f049d73c8c21e7e290a717a32d1be30
deandavidson <deandavidson37@gmail.com>
2023-05-07 22:52:24 -0700

    mostly done. need to add next and previous button

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -30,6 +30,25 @@ const Home = ({location}) => {
   }, [token]);
   console.log("one user data", user)
 
+  function renderElement() {
+    if (user.bio === null) {
+      return (<p>This user has no bio</p>)
+    }
+    else {
+      return (<p>{user.bio}</p>)
+    }
+  }
+
+  function renderTwitter() {
+    if (user.twitter_username === null) {
+      return (<p>This user has no twitter.</p>)
+    }
+    else {
+      return (<p>@{user.twitter_username}</p>)
+    }
+  }
+
+
 
   // return (
   //   <main>
@@ -82,19 +101,44 @@ const Home = ({location}) => {
           <div className="card-profile">
             <div className="card-left">
               <img className="card-profile-pic" src={user.avatar_url} alt="Avatar" />
-              <div className="card-left-bottom"></div>
+              <div className="card-left-bottom">
+                <p className="name">{user.name}</p>
+                <p className="username">@{user.login}</p>
+                <p className="location">{user.location}</p>
+                <a className="website-url">{user.html_url}</a>
+              </div>
             </div>
             <div className="card-right">
               <div className="card-right-top">
-                <div className="card-right-top-col"></div>
-                <div className="card-right-top-col"></div>
-                <div className="card-right-top-col"></div>
-                <div className="card-right-top-col"></div>
+                <div className="card-right-top-col">{user.public_repos} <p>Public Repos</p></div>
+                <div className="card-right-top-col">{user.public_gists} <p>Public Gists</p></div>
+                <div className="card-right-top-col" type="number">{user.followers} <p> Followers</p></div>
+                <div className="card-right-top-col">{user.following} <p> Following</p></div>
               </div>
               <div className="card-right-bottom">
+                <p className="headings">
+                  Biography: { user && renderElement() }
+                </p>
+                <p className="headings">
+                  Company: {user.company}
+                </p>
+                <p className="headings">
+                  Twitter: { user && renderTwitter() }
+                </p>
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          <button className="button-17-profile">
+              <Link
+                className="detail-link"
+                to={`/`}
+                state={user}
+                >
+                Profile List
+              </Link>
+          </button>
         </div>
       </div>
     </main>
